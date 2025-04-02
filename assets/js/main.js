53e2a7605ed22a66229f8d7b7cbf62c0ceea37e1
bannu akkabattula <bannuakkabattula@gmail.com>
2025-04-02 23:54:24 +0530

    updated all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -282,7 +282,7 @@
 		=        countdown	   =
 	=============================================*/
     function makeTimer() { 
-		var endTime = new Date("20 apr 2025 9:56:00 GMT+01:00");      
+		var endTime = new Date("26 jun 2025 9:56:00 GMT+01:00");      
 		endTime = (Date.parse(endTime) / 1000);
 		var now = new Date();
 		now = (Date.parse(now) / 1000);
